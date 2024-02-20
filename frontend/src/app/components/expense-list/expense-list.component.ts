1f1dd2bfaf6569b169b4cc2888ca88d8258511dc
tmh9x <thomas.hermanu@gmail.com>
2024-02-20 15:14:23 +0100

    add edit function to expenses

--- frontend/src/app/components/expense-list/expense-list.component.ts
+++ frontend/src/app/components/expense-list/expense-list.component.ts
@@ -3,25 +3,24 @@ import { CommonModule, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { ExpenseFormComponent } from '../expense-form/expense-form.component';
 import { ExpenseService } from '../../expense-service.service';
+import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
   standalone: true,
-  imports: [NgFor, ExpenseFormComponent, CommonModule],
+  imports: [NgFor, ExpenseFormComponent, CommonModule, FormsModule],
   selector: 'app-expense-list',
   templateUrl: './expense-list.component.html',
   styleUrls: ['./expense-list.component.css'],
 })
 export class ExpenseListComponent {
-  /* expensesList: any[] = []; */
   expensesData: any;
+  editingExpense: any;
 
   constructor(
     private expenseService: ExpenseService,
     private http: HttpClient
-  ) {
-    /* this.expensesList = this.expenseService.getExpensesList(); */
-  }
+  ) {}
 
   ngOnInit(): void {
     /* this.getExpensesList(); */
@@ -30,12 +29,25 @@ export class ExpenseListComponent {
     });
   }
 
-  /*   getExpensesList(): void {
-    this.expenseService.getExpensesList();
-  } */
-
   deleteExpense(id: number): void {
     this.expenseService.deleteExpense(id);
-    /*  this.expensesList = this.expenseService.getExpensesList(); */
+  }
+
+  openEditForm(expense: any): void {
+    this.editingExpense = { ...expense };
+  }
+
+  submitEditForm(): void {
+    this.expenseService
+      .editExpense(this.editingExpense.id, this.editingExpense)
+      .subscribe(
+        (response) => {
+          console.log('PUT request successful:', response);
+          window.location.reload();
+        },
+        (error) => {
+          console.error('Error making PUT request:', error);
+        }
+      );
   }
 }
