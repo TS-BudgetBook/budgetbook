b11583c49aac1481ecca2f01cc8d074d84e596f3
Marco Lindner <marco.lindner@gmx.org>
2024-04-09 12:28:39 +0200

    fix(ci): fixing linter issues

--- auth/src/auth/auth.service.ts
+++ auth/src/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { Request, Response } from 'express';
+import { Response } from 'express';
 import { User } from './entity/user.entity'
 import { UserService } from './user.service'
 import { JwtService, JwtSignOptions } from '@nestjs/jwt';
