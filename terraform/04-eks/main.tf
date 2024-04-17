3024ffe7163026afd9fb9d197e8c0ea510ee7a65
Marco Lindner <marco.lindner@gmx.org>
2024-04-17 12:27:48 +0200

    feat(terraform): enable admin permissions

--- terraform/04-eks/main.tf
+++ terraform/04-eks/main.tf
@@ -57,6 +57,8 @@ module "eks" {
   subnet_ids                     = module.vpc.private_subnets
   cluster_endpoint_public_access = true
 
+  enable_cluster_creator_admin_permissions = true
+
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
   }
